4028e6ddefa234d0a9dfddf67d4a5e9f80ee9a3b
Aryan Deshpande <dshryng@gmail.com>
2026-05-30 15:59:48 +0530

    feat: eventgrid and storage account

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,3 +22,22 @@ resource "azurerm_application_insights" "main" {
 
   application_type = "web"
 }
+
+resource "azurerm_eventgrid_topic" "main" {
+  name                = "cloudobs-events-dev"
+  location            = "Central India"
+  resource_group_name = azurerm_resource_group.main.name
+
+  input_schema = "EventGridSchema"
+}
+
+resource "azurerm_storage_account" "function_storage" {
+  name                = "cloudobsfuncdevsa"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = "Central India"
+
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  min_tls_version = "TLS1_2"
+}
